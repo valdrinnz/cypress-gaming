2619a63711b13798700056aa59f0b4d04e57d4b7
valdrinnz <valdrinnexus@gmail.com>
2024-11-25 02:01:15 +0100

    add login steps

--- cypress/pages/Home.ts
+++ cypress/pages/Home.ts
@@ -5,6 +5,7 @@ const categories = new Categories();
 export class Home {
   private readonly searchButton = 'button[data-element-description="nav-search-button"]';
   private readonly searchInput = 'input[data-element-description="search-games"]';
+  private readonly shopButton = 'button[data-element-description="nav-shop-button"]';
 
   public clickSearchButton(): void {
     cy.get(this.searchButton).click();
@@ -22,11 +23,38 @@ export class Home {
     });
   }
 
+  public clickShopButton(): void{
+    cy.get(this.shopButton).click();
+  }
+
+  public login(email: string, password: string): void {
+    this.clickSignInButton();
+    this.enterEmailInLoginForm(email);
+    this.enterPasswordInLoginForm(password);
+    this.submitCredentials();
+  }
+
   private searchForGameCategory(categoryName: string): void {
     cy.get(this.searchInput).type(categoryName);
   }
 
   private clearSearchCategoryInput(): void {
     cy.get(this.searchInput).clear();
+  }
+
+  private clickSignInButton(): void {
+    cy.get('button').contains('Sign in').click();
+  }
+
+  private enterEmailInLoginForm(email: string): void{
+    cy.get('#email').type(email)
+  }
+
+  private enterPasswordInLoginForm(password: string): void{
+    cy.get('#password').type(password)
+  }
+
+  private submitCredentials(): void{
+    cy.get('button').contains('Submit').click();
   }
 }
